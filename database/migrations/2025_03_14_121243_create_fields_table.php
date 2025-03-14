cd0d01b307af625f3e5a809820e81c25ae3ed8af
Gary Clarke <garyclarke1974@gmail.com>
2025-03-14 15:33:53 +0000

    Create models and migrations

---
+++ database/migrations/2025_03_14_121243_create_fields_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contact_id')
+                ->references('id')
+                ->on('contacts')
+                ->onDelete('cascade');
+            $table->string('key'); // Field name
+            $table->text('value'); // Field value
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fields');
+    }
+};
