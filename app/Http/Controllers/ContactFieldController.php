ed73c90a344ce85a5aacb308bc82db16779a86da
Gary Clarke <garyclarke1974@gmail.com>
2025-03-15 11:02:18 +0000

    Create controller and route

---
+++ app/Http/Controllers/ContactFieldController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\View\View;
+
+class ContactFieldController extends Controller
+{
+    public function show(Contact $contact): View
+    {
+        return view('contacts.fields.show', [
+            'contact' => $contact,
+            'fields' => $contact->fields
+        ]);
+    }
+}
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Database\Factories\ContactFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
     /** @use HasFactory<ContactFactory> */
     use HasFactory;
+
+    public function fields(): HasMany
+    {
+        return $this->hasMany(Field::class);
+    }
 }
